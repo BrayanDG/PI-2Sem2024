c5c64656019407521f959fdbb3f380815e3a7269
moscadro <pedrofranciscosj@gmail.com>
2024-05-19 10:23:20 -0300

    atualizados

--- usuarios/alunos/docs/gerartermo.php
+++ usuarios/alunos/docs/gerartermo.php
@@ -17,7 +17,7 @@
 <body>
 <div>
     <button>Salvar</button>
-    <a href="#">Ir para Programa de Atividade</a>
+    <a href="planodeatividade.php">Ir para Plano de Atividade</a>
 </div>
 <div id="container">
     <p>Pelo presente instrumento, as partes a seguir nomeadas e ao final assinadas, de um lado<?=$nomeFantasia ?>, inscrita no CNPJ sob o nº<?=$cnpj ?>, sita à rua <?=$ruaConcedente?>, doravante denominada CONCEDENTE, neste ato representada por <?=$representante ?>, <?=$cargoRepresentante?>, portador do CPF nº <?=$cpfRepresentante?> e, de outro lado, o(a) estudante________________ (NOME DO ESTAGIÁRIO), RG nº ____________, residente à _________________(ENDEREÇO DO ESTAGIÁRIO), na cidade de ___________, doravante denominado ESTAGIÁRIO (A), aluno (a) regularmente matriculado (a) no Curso Superior de Tecnologia em Desenvolvimento de Software Multiplataforma da Faculdade de Tecnologia de Itapira – Fatec “Ogari de Castro Pacheco”, inscrita no CNPJ sob o nº 62.823.257/0278-05, localizada na cidade de Itapira, Estado de São Paulo, doravante denominada INSTITUIÇÃO DE ENSINO, na condição de interveniente, acordam e estabelecem entre si as cláusulas e condições que regerão este TERMO DE COMPROMISSO DE ESTÁGIO OBRIGATÓRIO NÃO REMUNERADO. </p>
