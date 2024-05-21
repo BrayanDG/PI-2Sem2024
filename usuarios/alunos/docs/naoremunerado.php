167713c42d9b129be0391547f09594a6154212e4
moscadro <pedrofranciscosj@gmail.com>
2024-05-21 19:21:55 -0300

    Atualização Mila

--- usuarios/alunos/docs/naoremunerado.php
+++ usuarios/alunos/docs/naoremunerado.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    $cnpj = $_SESSION['cnpj'] ;
     $nomeFantasia = $_SESSION['nomeFantasia'] ;
     $representante = $_SESSION['representante'] ;
     $ruaConcedente = $_SESSION['ruaConcedente'] ;
